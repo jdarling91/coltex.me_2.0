a020eedb1eed6d8af71bdd1b176b826857dd5a4b
Jonathan Darling <jmdarling.tx@gmail.com>
2014-01-16 22:50:51 -0600

    More js changes.

--- js/site.js
+++ js/site.js
@@ -1,8 +1,7 @@
 $(document).ready( function() {
-  
-  $("#primary").hide();
-  
+  $("#primary").hide(); 
   $("#header").animo( { animation: 'bounceInLeft', duration: 1 }, function() {
     $("#primary").show().animo( { animation: 'bounceInRight', duration: 1 } );
   }); 
 });
+
